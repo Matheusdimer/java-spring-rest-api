ca85a5a557ab6aac9ee612699d71d1529e2a1e6d
Matheus de Lima Dimer <matheus.dimer@betha.com.br>
2021-04-19 10:42:13 -0300

    implements categoria domain

---
+++ src/main/java/com/betha/cursomc/domain/Categoria.java
@@ -0,0 +1,53 @@
+package com.betha.cursomc.domain;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Categoria {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String nome;
+
+    public Categoria() {
+
+    }
+
+    public Categoria(Integer id, String nome) {
+        this.id = id;
+        this.nome = nome;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Categoria categoria = (Categoria) o;
+        return id.equals(categoria.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
--- src/main/java/com/betha/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/betha/cursomc/resources/CategoriaResource.java
@@ -1,15 +1,24 @@
 package com.betha.cursomc.resources;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import com.betha.cursomc.domain.Categoria;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
     @GetMapping
-    public String getCategorias() {
-        return "Rest está funcionando";
+    public List<Categoria> getCategorias() {
+        Categoria categoria1 = new Categoria(1, "Informática");
+        Categoria categoria2 = new Categoria(2, "Escritório");
+
+        List<Categoria> lista = new ArrayList<>();
+        lista.add(categoria1);
+        lista.add(categoria2);
+
+        return lista;
     }
+
 }
