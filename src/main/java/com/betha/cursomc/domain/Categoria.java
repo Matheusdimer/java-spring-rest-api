9df0acb2eaf64601f255286a11fffb7fb4ad2c55
Matheus de Lima Dimer <matheus.dimer@betha.com.br>
2021-04-19 10:48:49 -0300

    implements categoria domain

--- src/main/java/com/betha/cursomc/domain/Categoria.java
+++ src/main/java/com/betha/cursomc/domain/Categoria.java
@@ -1,11 +1,13 @@
 package com.betha.cursomc.domain;
 
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import java.io.Serializable;
 import java.util.Objects;
 
+@Entity
 public class Categoria {
 
     @Id
