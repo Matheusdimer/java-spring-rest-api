a20db24b965a6a2b1ed78d186c3411c47ad6b108
Matheus de Lima Dimer <matheus.dimer@betha.com.br>
2021-05-05 08:59:20 -0300

    Primeiro upload de arquivo Amazon S3

--- src/main/java/com/betha/cursomc/CursomcApplication.java
+++ src/main/java/com/betha/cursomc/CursomcApplication.java
@@ -1,11 +1,22 @@
 package com.betha.cursomc;
 
+import com.betha.cursomc.services.S3Service;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CursomcApplication {
+public class CursomcApplication implements CommandLineRunner {
+	@Autowired
+	private S3Service s3Service;
+
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) {
+		s3Service.uploadFile("C:\\temp\\pdfteste.pdf");
 	}
 }
